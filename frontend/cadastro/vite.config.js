01bbd19ad3c4f28fa8dc9bf231ada77c8987bec2
Adrian Kayllan <adrian.profissional@proton.me>
2025-07-02 23:46:12 -0500

    #

--- frontend/cadastro/vite.config.js
+++ frontend/cadastro/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'C:/Users/PC HOME/React-em-Django/backend/home/static/js',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/main.jsx'),
+      output: {
+        entryFileNames: 'main.js', 
+        assetFileNames: '[name].[ext]' 
+      }
+    }
   }
-})
+});
