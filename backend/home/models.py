c541c2d0cece9adefb9d8e6f70d105c5975ef220
Adrian Kayllan <adrian.profissional@proton.me>
2025-07-02 22:51:53 -0500

    #

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=255)
+    idade = models.IntegerField(max_length=2)
+    email = models.CharField(max_length=255)
+
+    def __str__():
+        return Pessoa.nome
